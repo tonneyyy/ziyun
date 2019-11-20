c58f74b56ff5b80f1599449b70217f3faa221b27
wjh121 <1353591662@qq.com>
2019-11-20 17:16:44 +0800

    wjh

--- src/main/java/com/hxzy/controller/admin/LoginController.java
+++ src/main/java/com/hxzy/controller/admin/LoginController.java
@@ -6,10 +6,7 @@ import com.hxzy.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -29,7 +26,7 @@ public class LoginController {
 
     @ResponseBody
     @PostMapping(value = "/loginExecute")
-    public ResponseMessage loginExecute(HttpSession session, Teacher teacher){
-        return this.teacherService.login(session,teacher);
+    public ResponseMessage loginExecute(HttpSession session, Teacher teacher,@RequestParam("account") String name){
+        return this.teacherService.login(session,teacher,name);
     }
 }
